1c3afd6fea7d353ccde081a26473dd0984a39240
Lucas Puf <lucas.puf.lp@gmail.com>
2025-01-07 14:39:26 +0100

    🟢 Added calculate button and implemented departure time calculation; updated styles and input handling

--- JS/oneDayOVTScript.js
+++ JS/oneDayOVTScript.js
@@ -1,13 +1,58 @@
-/* Get Input values*/
-const arrivalInput = document.getElementById("#input_Arrival").value;
-const breakInput = document.getElementById("#input_Break").value;
-const overtimeInput = document.getElementById("#input_Overtime").value;
+/* Get Inputs*/
+const arrivalInput = document.getElementById("input_Arrival");
+const breakInput = document.getElementById("input_Break");
+const overtimeInput = document.getElementById("input_Overtime");
 
 /* Function to detect if all Inputs have values */
-const checkInputs = function (arrivalInput, breakInput, overtimeInput) {
+/* const checkInputs = function (arrivalInput, breakInput, overtimeInput) {
   if (!arrivalInput == "" || breakInput == "" || overtimeInput == "") {
-    return true;
-  } else {
     return false;
+  } else {
+    return true;
   }
+}; */
+
+/* Functions to convert time to minutes and minutes to time */
+function timeStringToMinutes(timeString) {
+  const [hours, minutes] = timeString.split(":").map(Number);
+  return hours * 60 + minutes;
+}
+
+function minutesToTimeString(minutes) {
+  const hours = Math.floor(minutes / 60)
+    .toString()
+    .padStart(2, "0");
+  const mins = (minutes % 60).toString().padStart(2, "0");
+  return `${hours}:${mins}`;
+}
+
+/* Function to calculate the departure Time */
+const calcDepartureTime = function (arrivalInput, breakInput, overtimeInput) {
+  /* if (!checkInputs(arrivalInput, breakInput, overtimeInput)) {
+    return console.error("All fields must be filled!");
+  } else { */
+  const arrivalMinutes = timeStringToMinutes(arrivalInput.value);
+  const breakMinutes = timeStringToMinutes(breakInput.value);
+  const overtimeMinutes = timeStringToMinutes(overtimeInput.value);
+  let calcDepartureTime;
+
+  calcDepartureTime = arrivalMinutes + overtimeMinutes + breakMinutes + 450;
+  const departureTime = minutesToTimeString(calcDepartureTime);
+  return departureTime;
+  /* } */
 };
+
+/* Function to display the departure Time */
+const departureOutput = document.querySelector(".departureTime");
+const displayTime = function () {
+  const departureTime = calcDepartureTime(
+    arrivalInput,
+    breakInput,
+    overtimeInput
+  );
+  departureOutput.textContent = departureTime || "00:00";
+};
+
+/* Event Listener for the calculater Button */
+const calculateBttn = document.querySelector(".calculateBttn");
+calculateBttn.addEventListener("click", displayTime);
