48140079383d9ecca92ad9d585c1b99ab9ec3c1f
Lucas Puf <lucas.puf.lp@gmail.com>
2025-01-09 12:53:27 +0100

    🟢 Refactored time display functionality into a new module; updated HTML structure for date and time display; improved CSS styling for dropdown menu and current date/time elements

---
+++ JS/timeDisplay.js
@@ -0,0 +1,52 @@
+/**
+ * Returns the current date formatted as "Weekday, MM/DD/YYYY".
+ *
+ * @returns {string} The current date in the format "Weekday, MM/DD/YYYY".
+ */
+function getCurrentDate() {
+  const weekdays = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  const currentDate = new Date();
+  const currentDateLayout =
+    weekdays[currentDate.getDay()] + ", " + currentDate.toLocaleDateString();
+
+  return currentDateLayout;
+}
+
+/**
+ * Gets the current time as a localized string.
+ *
+ * @returns {string} The current time formatted as a locale-specific string.
+ */
+function getCurrentTime() {
+  const currentTime = new Date();
+  return currentTime.toLocaleTimeString();
+}
+
+/**
+ * Updates the text content of the HTML element with the class "currentTime"
+ * to the current time.
+ */
+function updateCurrentTime() {
+  const currentTimeH = document.querySelector(".currentTime");
+  currentTimeH.textContent = getCurrentTime();
+}
+
+/**
+ * A reference to the HTML element with the class "currentDate".
+ * This element is used to display the current date.
+ * @type {Element}
+ */
+document.addEventListener("DOMContentLoaded", () => {
+  const currentDateH = document.querySelector(".currentDate");
+  currentDateH.textContent = getCurrentDate();
+  updateCurrentTime();
+  setInterval(updateCurrentTime, 1000);
+});
