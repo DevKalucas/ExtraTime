7d69068d2863684ee656eff2bc66134ce3560d89
Lucas Puf <lucas.puf.lp@gmail.com>
2025-01-24 14:15:05 +0100

    🔧 Remove unused time calculation functions from app.js

--- JS/app.js
+++ JS/app.js
@@ -1,18 +1,4 @@
-/////////////////
-// Time Calculation
-
-function timeStringToMinutes(timeString) {
-  const [hours, minutes] = timeString.split(":").map(Number);
-  return hours * 60 + minutes;
-}
-
-function minutesToTimeString(minutes) {
-  const hours = Math.floor(minutes / 60)
-    .toString()
-    .padStart(2, "0");
-  const mins = (minutes % 60).toString().padStart(2, "0");
-  return `${hours}:${mins}`;
-}
+// NOT USED
 
 // BUG If first time no input abd
 function calcDepartureTime(day) {
