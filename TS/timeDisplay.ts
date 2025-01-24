6fd2bf7dd53eaaaf6cc8fd58e2ef2dc0b1ce641c
Lucas Puf <lucas.puf.lp@gmail.com>
2025-01-24 11:50:48 +0100

    🔧 Refactor timeDisplay functions to use var instead of const and add TypeScript version

--- TS/timeDisplay.ts
+++ TS/timeDisplay.ts
@@ -4,7 +4,7 @@
  * @returns {string} The current date in the format "Weekday, MM/DD/YYYY".
  */
 function getCurrentDate() {
-  var weekdays = [
+  const weekdays = [
     "Sunday",
     "Monday",
     "Tuesday",
@@ -13,35 +13,39 @@ function getCurrentDate() {
     "Friday",
     "Saturday",
   ];
-  var currentDate = new Date();
-  var currentDateLayout =
+  const currentDate = new Date();
+  const currentDateLayout =
     weekdays[currentDate.getDay()] + ", " + currentDate.toLocaleDateString();
+
   return currentDateLayout;
 }
+
 /**
  * Gets the current time as a localized string.
  *
  * @returns {string} The current time formatted as a locale-specific string.
  */
 function getCurrentTime() {
-  var currentTime = new Date();
+  const currentTime = new Date();
   return currentTime.toLocaleTimeString();
 }
+
 /**
  * Updates the text content of the HTML element with the class "currentTime"
  * to the current time.
  */
 function updateCurrentTime() {
-  var currentTimeH = document.querySelector(".currentTime");
+  const currentTimeH = document.querySelector(".currentTime") as HTMLElement;
   currentTimeH.textContent = getCurrentTime();
 }
+
 /**
  * A reference to the HTML element with the class "currentDate".
  * This element is used to display the current date.
  * @type {Element}
  */
-document.addEventListener("DOMContentLoaded", function () {
-  var currentDateH = document.querySelector(".currentDate");
+document.addEventListener("DOMContentLoaded", () => {
+  const currentDateH = document.querySelector(".currentDate") as HTMLElement;
   currentDateH.textContent = getCurrentDate();
   updateCurrentTime();
   setInterval(updateCurrentTime, 1000);
