47b39fd49a45ed40c0bd324a20608696de4beed7
Lucas Puf <lucas.puf.lp@gmail.com>
2025-01-24 13:21:34 +0100

    🔧 Migrate time input handling to TypeScript and remove redundant JavaScript code

---
+++ TS/weeklyOVTScript.ts
@@ -0,0 +1,74 @@
+////////////////////////////
+// Import Inputs
+
+// Monday
+const arrivalMOInput = document.getElementById(
+  "input_Monday_Arrival"
+) as HTMLInputElement;
+const breakMOInput = document.getElementById(
+  "input_Monday_Break"
+) as HTMLInputElement;
+const overtimeMOInput = document.getElementById(
+  "input_Monday_Overtime"
+) as HTMLInputElement;
+
+// Tuesday
+const arrivalTUInput = document.getElementById(
+  "input_Tuesday_Arrival"
+) as HTMLInputElement;
+const breakTUInput = document.getElementById(
+  "input_Tuesday_Break"
+) as HTMLInputElement;
+const overtimeTUInput = document.getElementById(
+  "input_Tuesday_Overtime"
+) as HTMLInputElement;
+
+// Wednesday
+const arrivalWEInput = document.getElementById(
+  "input_Wednesday_Arrival"
+) as HTMLInputElement;
+const breakWEInput = document.getElementById(
+  "input_Wednesday_Break"
+) as HTMLInputElement;
+const overtimeWEInput = document.getElementById(
+  "input_Wednesday_Overtime"
+) as HTMLInputElement;
+
+// Thursday
+const arrivalTHInput = document.getElementById(
+  "input_Thursday_Arrival"
+) as HTMLInputElement;
+const breakTHInput = document.getElementById(
+  "input_Thursday_Break"
+) as HTMLInputElement;
+const overtimeTHInput = document.getElementById(
+  "input_Thursday_Overtime"
+) as HTMLInputElement;
+
+// Friday
+const arrivalFRInput = document.getElementById(
+  "input_Friday_Arrival"
+) as HTMLInputElement;
+const breakFRInput = document.getElementById(
+  "input_Friday_Break"
+) as HTMLInputElement;
+const overtimeFRInput = document.getElementById(
+  "input_Friday_Overtime"
+) as HTMLInputElement;
+
+function getArrivalTime(day: string) {
+  switch (day) {
+    case "monday":
+      return arrivalMOInput.value || "00:00";
+    case "tuesday":
+      return arrivalTUInput.value || "00:00";
+    case "wednesday":
+      return arrivalWEInput.value || "00:00";
+    case "thursday":
+      return arrivalTHInput.value || "00:00";
+    case "friday":
+      return arrivalFRInput.value || "00:00";
+    default:
+      throw new Error("Could not find day");
+  }
+}
